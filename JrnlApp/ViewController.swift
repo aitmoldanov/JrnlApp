ca9b9c6cd50e48734888c030ea6cbe850cf3e0d6
Abzal <abzalmanatovich@icloud.com>
2020-09-22 15:52:51 +0600

    Initial Commit

---
+++ JrnlApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JrnlApp
+//
+//  Created by Абзал Айтмолданов on 9/22/20.
+//  Copyright © 2020 Абзал Айтмолданов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
